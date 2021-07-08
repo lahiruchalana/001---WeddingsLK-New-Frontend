a9c555eef1e869c1b1d30219c87bc9bc62885b83
W.D. Lahiru Chalana Iresh <55834083+lahiruchalana@users.noreply.github.com>
2021-07-08 23:34:47 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,131 +1,35 @@
-import React from "react";
+import React from 'react';
+// import { BrowserRouter as Router } from 'react-router-dom'
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import './App.css'
-import Header from "./Components/Header.js"
-import Footer from './Components/Footer.js'
-import Home from './Components/Home.js'
-import About from "./Components/About.js"
-import Galleries from "./Components/Galleries.js"
-import SearchResults from "./Components/SearchResults.js"
-import Services from "./Components/Services.js"
-import UserProfile from "./Components/UserProfile.js"
-import UserAuth from "./Components/UserAuth.js"
-import Vendors from "./Components/Vendors.js"
-import WeddingPlans from "./Components/WeddingPlans.js"
-import AdminLogin from "./Components/AdminLogin.js"
-import EmployeeLogin from "./Components/EmployeeLogin.js"
-import SideBarUser from "./Components/SideBarUser.js";
-import AdminProfile from "./Components/AdminProfile";
-import EmployeeProfile from "./Components/EmployeeProfile";
-import SideBarEmployee from "./Components/SideBarEmployee";
-import SideBarAdmin from "./Components/SideBarAdmin";
-import HeaderAdmin from "./Components/HeaderAdmin";
-import HeaderEmployee from "./Components/HeaderEmployee";
-import Shop from "./Components/Shop";
-import Websites from "./Components/Websites";
-import InvitationCards from "./Components/InvitationCards";
-import CommunityFeedBacks from "./Components/CommunityFeedBacks";
+import { DataProvider } from './GlobalState'
+import HeaderDev from './components/headers/Header'
+import Header from './components/header/Header';
+import MainPages from './components/mainpages/Pages'
+import Home from './components/home/Home.js';
 
 function App() {
   return (
-    //BEM
     <Router>
-      <div className="App">
-        <Switch>
-          <Route path="/about">
-            <Header/> 
-            <About/>
-            <Footer/>        
+      <Switch>
+        <Route path="/home">
+          <Header />
+          <Home />
+        </Route>
+        {/* <Route path="/home">
+          <Header />
+          <Home />
+        </Route> */}
+        <DataProvider>
+          <Route path="/">
+            {/* <Header /> */}
+            <div className="App">
+              <HeaderDev />
+              <MainPages />
+            </div>
           </Route>
-          <Route path="/Galleries">
-            <Header/> 
-            <Galleries/>
-            <Footer/>        
-          </Route>
-          <Route path="/search/results">
-            <Header/> 
-            <SearchResults/>
-            <Footer/>        
-          </Route>
-          <Route path="/services">
-            <Header/> 
-            <Services/>
-            <Footer/>        
-          </Route>
-          <Route path="/user/profile">
-            <SideBarUser />
-            <Header/>
-            <UserProfile/> 
-          </Route>
-          <Route path="/user/auth">
-            <Header/> 
-            <UserAuth/>
-            <Footer/>        
-          </Route>
-          <Route path="/vendors">
-            <Header/> 
-            <Vendors/>
-            <Footer/>        
-          </Route>
-          <Route path="/weddingplans">
-            <Header/> 
-            <WeddingPlans/>
-            <Footer/>        
-          </Route>
-          <Route path="/admin/profile">
-            <SideBarAdmin/>
-            <HeaderAdmin/>
-            <AdminProfile/>    
-          </Route>
-          <Route path="/admin/login">
-            <Header/> 
-            <AdminLogin/>
-            <Footer/>        
-          </Route>
-          <Route path="/employee/profile">
-            <SideBarEmployee/>
-            <HeaderEmployee/>
-            <EmployeeProfile/>     
-          </Route>
-          <Route path="/employee/login">
-            <Header/> 
-            <EmployeeLogin/>
-            <Footer/>        
-          </Route>
-          <Route path="/shops">
-            <Header/> 
-            <Shop/>
-            <Footer/>        
-          </Route>
-          <Route path="/weddingwebsites">
-            <Header/> 
-            <Websites/>
-            <Footer/>        
-          </Route>
-          <Route path="/weddinginvitationcards">
-            <Header/> 
-            <InvitationCards/>
-            <Footer/>        
-          </Route>
-          <Route path="/communityfeedbacks">
-          <Header/>
-            <CommunityFeedBacks/>
-       
-          </Route>
-          <Route path="/search/reasults">
-            <Header/> 
-            <SearchResults />
-            <Footer/>        
-          </Route>
-        </Switch>
-        <Route exact path="/">
-            <Header /> 
-            <Home />
-            <Footer />        
-          </Route>
-      </div>
+        </DataProvider>
+      </Switch>
     </Router>
-    
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,48 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-/// new imports for react-redux issue in useDispatch start ////
-
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';
-
-import { reducers } from './Reducers';
 
 
-const store = createStore(reducers, compose(applyMiddleware(thunk)));
-
-//// new imports for react redux issue in useDispatch end //// 
-
-
-ReactDOM.render(
-
-  /// START -> MERN tutorial 12 hour video first video -. why i include this here ////
-
-  // <React.StrictMode>
-  //   <App />
-  // </React.StrictMode>,
-  // document.getElementById('root'),
-
-  /// END -> MERN tutorial 12 hour video first video -. why i include this here ////
-
-
-
-  //// new imports for react redux issue in useDispatch start //// 
-
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root'),
-
-  //// new imports for react redux issue in useDispatch end //// 
-  
+ReactDOM.render(<App />,
+  document.getElementById('root')
 );
 
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
